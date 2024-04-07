3cc24859214553eb002f29385859364cb927c8af
Jose Baroni <josebaroni@turno.com>
2024-04-07 16:27:05 -0300

    testing next-seo 3

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -3,6 +3,7 @@ import React from "react";
 import {createTheme, MantineProvider} from '@mantine/core';
 import DataProvider from "@/DataProvider";
 import {Inter} from "next/font/google";
+import {NextSeo} from "next-seo";
 
 const font = Inter({weight: ["300", "500", "700", "900"], subsets: ["latin"]})
 
@@ -12,14 +13,33 @@ export default function App({Component, pageProps}) {
     });
 
     return (
-        <MantineProvider
-            withGlobalStyles
-            withNormalizeCSS
-            theme={theme}
-        >
-            <DataProvider>
-                <Component {...pageProps} />
-            </DataProvider>
-        </MantineProvider>
+        <React.Fragment>
+            <NextSeo
+                title="ScrumBluff - Free Planning Poker Tool"
+                description="Boost your team's productivity with Scrum Bluff, the top free online planning poker tool. Create virtual rooms, discuss topics, and enhance collaboration with ease."
+                openGraph={{
+                    url: "https://scrumbluff.com",
+                    title: "ScrumBluff - Free Planning Poker Tool",
+                    description: "Boost your team's productivity with Scrum Bluff, the top free online planning poker tool. Create virtual rooms, discuss topics, and enhance collaboration with ease.",
+                    siteName: "ScrumBluff",
+                    images: [
+                        {url: 'https://scrumbluff.com/social-card.png'}
+                    ],
+                }}
+                twitter={{
+                    cardType: "summary_large_image",
+                    site: "https://scrumbluff.com"
+                }}
+            />
+            <MantineProvider
+                withGlobalStyles
+                withNormalizeCSS
+                theme={theme}
+            >
+                <DataProvider>
+                    <Component {...pageProps} />
+                </DataProvider>
+            </MantineProvider>
+        </React.Fragment>
     )
 }
--- frontend/src/pages/_document.js
+++ frontend/src/pages/_document.js
@@ -1,27 +1,9 @@
 import {Head, Html, Main, NextScript} from "next/document";
-import {NextSeo} from "next-seo";
 import React from "react";
 
 export default function Document() {
     return (
         <Html lang="en">
-            <NextSeo
-                title="ScrumBluff - Free Planning Poker Tool"
-                description="Boost your team's productivity with Scrum Bluff, the top free online planning poker tool. Create virtual rooms, discuss topics, and enhance collaboration with ease."
-                openGraph={{
-                    url: "https://scrumbluff.com",
-                    title: "ScrumBluff - Free Planning Poker Tool",
-                    description: "Boost your team's productivity with Scrum Bluff, the top free online planning poker tool. Create virtual rooms, discuss topics, and enhance collaboration with ease.",
-                    siteName: "ScrumBluff",
-                    images: [
-                        {url: 'https://scrumbluff.com/social-card.png'}
-                    ],
-                }}
-                twitter={{
-                    cardType: "summary_large_image",
-                    site: "https://scrumbluff.com"
-                }}
-            />
             <Head/>
             <body>
             <Main/>
